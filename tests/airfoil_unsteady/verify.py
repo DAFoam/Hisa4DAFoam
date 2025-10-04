ea61ad6696a74a6923b817b350d2ba71dd131435
Ping He <phe@iastate.edu>
2025-10-04 10:06:08 -0500

    Added an unsteady test.

---
+++ tests/airfoil_unsteady/verify.py
@@ -0,0 +1,17 @@
+f = open("log.txt", "r")
+lines = f.readlines()
+f.close()
+
+CD = []
+for line in lines:
+    if "Cd       :" in line:
+        cols = line.split()
+        CD.append(float(cols[2]))
+print(CD)
+CD_Final = CD[-1]
+
+if abs(CD_Final - 0.018972614) / 0.018972614 > 1e-7:
+    print("HiSA test failed!")
+    exit(1)
+else:
+    print("HiSA test passed!")
